4c585d266a87d1675aeca829c03a4685d71e2d5d
hsytkm <t_hosoya1214@yahoo.co.jp>
2019-08-11 18:12:15 +0900

    Update xaml

--- ExplorerAddressBar2/Views/DirectoryPathBar.xaml.cs
+++ ExplorerAddressBar2/Views/DirectoryPathBar.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System.Windows.Controls;
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
 
 namespace ExplorerAddressBar2.Views
 {
@@ -7,9 +9,31 @@ namespace ExplorerAddressBar2.Views
     /// </summary>
     public partial class DirectoryPathBar : UserControl
     {
+        private double windowWidth;
+        private double myControlWidth;
+
         public DirectoryPathBar()
         {
             InitializeComponent();
+
+#if true    // サイズ確認
+            PathBarBody.SizeChanged += (sender, e) =>
+            {
+                myControlWidth = e.NewSize.Width;
+            };
+
+            this.Loaded += (_, __) =>
+            {
+                var window = Window.GetWindow(this);
+                window.SizeChanged += (sender, e) =>
+                {
+                    windowWidth = e.NewSize.Width;
+                    Console.WriteLine($"DirectoryPathBar Width: {myControlWidth:f2} / {windowWidth:f2}");
+                };
+            };
+#endif
+
         }
+
     }
 }
